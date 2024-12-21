9a332d233e81d5d22059b8057a5d33ad869f2fff
Inès Robin <inesrobin@Iness-MBP.communityfibre.co.uk>
2024-12-21 18:59:25 +0000

    Rough UI setup

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD: MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private RectTransform healthBar;
+    [SerializeField] private Image lifeIcon;
+}
